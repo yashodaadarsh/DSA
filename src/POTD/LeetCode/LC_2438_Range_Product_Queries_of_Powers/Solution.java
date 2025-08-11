e00870e0fc3ab8b41a7fad01750211f45155220e
= <=>
2025-08-11 23:58:02 +0530

    POTD: ( Aug-11 ) Leetcode 2438 - Range Product Queries of Powers

---
+++ src/POTD/LeetCode/LC_2438_Range_Product_Queries_of_Powers/Solution.java
@@ -0,0 +1,35 @@
+package POTD.LeetCode.LC_2438_Range_Product_Queries_of_Powers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    private final int MOD = 1000000000 + 7;
+
+    public int[] productQueries(int n, int[][] queries) {
+        int ql = queries.length;
+        int[] res = new int[ql];
+        List<Integer> powers = new ArrayList<>();
+
+        // Step 1: Build powers array
+        for (int i = 0; i < 31; i++) {
+            if (((1 << i) & n) != 0) {
+                powers.add(1 << i);
+            }
+        }
+
+        // Step 2: Answer queries
+        int k = 0;
+        for (int[] query : queries) {
+            int st = query[0];
+            int end = query[1];
+            long prod = 1;
+
+            for (int i = st; i <= end; i++) {
+                prod = (prod * powers.get(i)) % MOD;
+            }
+            res[k++] = (int) prod;
+        }
+        return res;
+    }
+}
