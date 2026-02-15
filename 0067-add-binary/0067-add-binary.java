14e57953dabc04636f5e2530ce23b9d5b735917a
yashodaadarsh <147117697+yashodaadarsh@users.noreply.github.com>
2026-02-15 21:22:01 +0530

    Time: 1 ms (99.65%), Space: 43.6 MB (70.73%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,68 +1,92 @@
+// class Solution {
+//     public String addBinary(String a, String b) {
+//         int N = Math.min( a.length(), b.length() );
+//         int i = 0;
+//         StringBuilder sb = new StringBuilder();
+//         int carry = 0;
+//         int al = a.length();
+//         int bl = b.length();
+//         while( i < N ){
+//             int n1 = a.charAt(al-1-i)-'0';
+//             int n2 = b.charAt(bl-1-i)-'0';
+//             int val = n1+n2+carry;
+//             // System.out.println(val);
+//             if( val == 2 ){
+//                 sb.append("0");
+//                 carry = 1;
+//             }
+//             else if( val == 3 ){
+//                 sb.append("1");
+//                 carry = 1;
+//             }
+//             else{
+//                 sb.append(""+val);
+//                 carry = 0;
+//             }
+//             i++;
+//         }
+//         int j = a.length()-i-1;
+//         while( j >= 0 && a.length() > N ){
+//             int n1 = a.charAt(j)-'0';
+//             int val = n1+carry;
+//             System.out.println(val);
+//             if( val == 2 ){
+//                 sb.append("0");
+//                 carry = 1;
+//             }
+//             else if( val == 3 ){
+//                 sb.append("1");
+//                 carry = 1;
+//             }
+//             else{
+//                 sb.append(""+val);
+//                 carry = 0;
+//             }
+//             j--;
+//         }
+//         j = b.length()-i-1;
+//         while( j >= 0 && b.length() > N ){
+//             int n1 = b.charAt(j)-'0';
+//             int val = n1+carry;
+//             if( val == 2 ){
+//                 sb.append("0");
+//                 carry = 1;
+//             }
+//             else if( val == 3 ){
+//                 sb.append("1");
+//                 carry = 1;
+//             }
+//             else{
+//                 sb.append(""+val);
+//                 carry = 0;
+//             }
+//             j--;
+//         }
+//         if( carry != 0 ) sb.append(carry+"");
+//         return sb.reverse().toString();
+//     }
+// }
+
+//Optimal
 class Solution {
     public String addBinary(String a, String b) {
-        int N = Math.min( a.length(), b.length() );
-        int i = 0;
-        StringBuilder sb = new StringBuilder();
+
+        StringBuilder res = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         int carry = 0;
-        int al = a.length();
-        int bl = b.length();
-        while( i < N ){
-            int n1 = a.charAt(al-1-i)-'0';
-            int n2 = b.charAt(bl-1-i)-'0';
-            int val = n1+n2+carry;
-            // System.out.println(val);
-            if( val == 2 ){
-                sb.append("0");
-                carry = 1;
-            }
-            else if( val == 3 ){
-                sb.append("1");
-                carry = 1;
-            }
-            else{
-                sb.append(""+val);
-                carry = 0;
-            }
-            i++;
+
+        while(i >= 0 || j >= 0 || carry == 1) {
+
+            int sum = carry;
+
+            if(i >= 0) sum += a.charAt(i--) - '0';
+            if(j >= 0) sum += b.charAt(j--) - '0';
+
+            res.append(sum % 2);
+            carry = sum / 2;
         }
-        int j = a.length()-i-1;
-        while( j >= 0 && a.length() > N ){
-            int n1 = a.charAt(j)-'0';
-            int val = n1+carry;
-            System.out.println(val);
-            if( val == 2 ){
-                sb.append("0");
-                carry = 1;
-            }
-            else if( val == 3 ){
-                sb.append("1");
-                carry = 1;
-            }
-            else{
-                sb.append(""+val);
-                carry = 0;
-            }
-            j--;
-        }
-        j = b.length()-i-1;
-        while( j >= 0 && b.length() > N ){
-            int n1 = b.charAt(j)-'0';
-            int val = n1+carry;
-            if( val == 2 ){
-                sb.append("0");
-                carry = 1;
-            }
-            else if( val == 3 ){
-                sb.append("1");
-                carry = 1;
-            }
-            else{
-                sb.append(""+val);
-                carry = 0;
-            }
-            j--;
-        }
-        if( carry != 0 ) sb.append(carry+"");
-        return sb.reverse().toString();
+
+        return res.reverse().toString();
     }
 }
