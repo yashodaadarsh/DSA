ce7ea8c99eef0d15681fb0a003e98a9cbd0400b3
yashodaadarsh <147117697+yashodaadarsh@users.noreply.github.com>
2026-02-24 22:26:37 +0530

    Time: 50 ms (44.53%), Space: 118.2 MB (28.89%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,15 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n+2][2][3];
         int k = 2;
+        int[][][] dp = new int[n+2][2][k+1];
 
-        for( int i = 0; i < n; i++ ){
-            for( int j = 0; j < k; j++ )
-                Arrays.fill( dp[i][j] ,-1 );
+        // for( int i = 0; i < n; i++ ){
+        //     for( int j = 0; j < k; j++ )
+        //         Arrays.fill( dp[i][j] ,-1 );
+        // }
+
+        for( int i = n-1; i >= 0; i-- ){
+            for( int lt = 1; lt <= k; lt++ ){
+                dp[i][1][lt] = Math.max( -prices[i] + dp[i+1][0][lt], dp[i+1][1][lt] );
+                dp[i][0][lt] = Math.max( prices[i] + dp[i+1][1][lt-1], dp[i+1][0][lt] );
+            }
         }
 
-        return solve( 0,n,1,k,prices,dp );
+        return dp[0][1][k];
     }
 
     private int solve( int ind, int n, int buy, int k, int[] prices, int[][][] dp ){
