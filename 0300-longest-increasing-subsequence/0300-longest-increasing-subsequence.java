5565a0e9e58ec7ef7a5a4610889e666d983f221e
yashodaadarsh <147117697+yashodaadarsh@users.noreply.github.com>
2026-02-25 00:31:43 +0530

    Time: 6 ms (87.12%), Space: 46.3 MB (55.04%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,21 +1,54 @@
 class Solution {
+
+    private int findIdx( ArrayList<Integer> arr, int target ){
+        int st = 0;
+        int end = arr.size() - 1;
+        int ans = -1;
+        while( st <= end ){
+            int mid = ( end-st )/2 + st;
+            if( arr.get(mid) >= target ){
+                end = mid - 1;
+                ans = mid;
+            }
+            else{
+                st = mid + 1;
+            }
+        }
+        return ans;
+    }
+
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
-        int[] dp = new int[n+1];
-        Arrays.fill( dp,1 );
-        int max = 1;
 
+        // LIS Using BinarySearch
+        ArrayList<Integer> seqSize = new ArrayList<>();
         for( int i = 0; i < n; i++ ){
-            for( int j = 0; j < i; j++ ){
-                if( nums[j] < nums[i] ){
-                    dp[i] = Math.max( dp[i], 1 + dp[j] );
-                    max = Math.max( max,dp[i] );
-                }
+            if( seqSize.isEmpty() || seqSize.get( seqSize.size() - 1 ) < nums[i] ){
+                seqSize.add( nums[i] );
+            }
+            else{
+                int idx = findIdx( seqSize,nums[i] );
+                seqSize.set( idx, nums[i] );
             }
         }
 
-        return max;
+        return seqSize.size();
+
+        // int[] dp = new int[n+1];
+        // Arrays.fill( dp,1 );
+        // int max = 1;
+
+        // for( int i = 0; i < n; i++ ){
+        //     for( int j = 0; j < i; j++ ){
+        //         if( nums[j] < nums[i] ){
+        //             dp[i] = Math.max( dp[i], 1 + dp[j] );
+        //             max = Math.max( max,dp[i] );
+        //         }
+        //     }
+        // }
+
+        // return max;
 
         // int[][] dp = new int[n][n+1];
         // for( int i = 0; i < n; i++ ){
