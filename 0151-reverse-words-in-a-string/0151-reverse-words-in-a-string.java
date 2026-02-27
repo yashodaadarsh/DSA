0ff9e0de193040f3dd422b5c861e612b889af6a1
yashodaadarsh <147117697+yashodaadarsh@users.noreply.github.com>
2026-02-27 10:28:28 +0530

    Time: 4 ms (93.66%), Space: 43.9 MB (96.75%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,27 +1,29 @@
 class Solution {
     public String reverseWords(String s) {
-        ArrayList<String> list = new ArrayList<>();
+
         int n = s.length();
-        int i = 0;
-        while( i < n ){
-            while( i < n && s.charAt(i) == ' ' ) i++;
-            StringBuilder sb = new StringBuilder();
-            while( i < n && s.charAt(i) != ' ' ){
-                sb.append( s.charAt(i) );
-                i++;
+        int i = n-1;
+        StringBuilder sb = new StringBuilder();
+
+        // read from the end and append to the result
+        while( i >= 0 ){
+            while( i >= 0 && s.charAt(i) == ' ' ){
+                i--;
             }
-            list.add( sb.toString() );
-            while( i < n && s.charAt(i) == ' ' ) i++;
-            // i++;
+            if( i < 0 ) break;
+            int end = i;
+
+            while( i >= 0 && s.charAt(i) != ' ' ){
+                i--;
+            }
+
+            if( !sb.isEmpty() ){
+                sb.append( " " );
+            }
+
+            sb.append( s.substring(i+1,end+1) );
         }
 
-        StringBuilder ans = new StringBuilder();
-        int size = list.size();
-        for( int j = size - 1; j >= 0; j-- ){
-            ans.append( list.get(j) );
-            if( j != 0 )
-                ans.append( " " );
-        }
-        return ans.toString();
+        return sb.toString();
     }
 }
