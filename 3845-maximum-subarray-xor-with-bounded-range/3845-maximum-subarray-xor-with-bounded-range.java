8fafb79f05eca9bc710c628b99ae6e07daa731aa
yashodaadarsh <147117697+yashodaadarsh@users.noreply.github.com>
2026-03-01 17:04:13 +0530

    Time: 172 ms (55.23%), Space: 65.2 MB (82.15%) - LeetHub

--- 3845-maximum-subarray-xor-with-bounded-range/3845-maximum-subarray-xor-with-bounded-range.java
+++ 3845-maximum-subarray-xor-with-bounded-range/3845-maximum-subarray-xor-with-bounded-range.java
@@ -1,73 +1,75 @@
 class Solution {
     public int maxXor(int[] nums, int k) {
-
         int n = nums.length;
+        int[] prefixXor = new int[n];
+        prefixXor[0] = nums[0];
 
-        // Proper prefix array
-        int[] pref = new int[n + 1];
-        for (int i = 0; i < n; i++) {
-            pref[i + 1] = pref[i] ^ nums[i];
+        for( int i = 1; i < n; i++ ){
+            prefixXor[i] = prefixXor[i-1] ^ nums[i];
         }
 
         Deque<Integer> minD = new ArrayDeque<>();
         Deque<Integer> maxD = new ArrayDeque<>();
-
         Trie trie = new Trie();
-        trie.insert(0);  // very important
+        trie.insert(0);
 
         int l = 0;
-        int ans = 0;
+        int ans = Integer.MIN_VALUE;
 
-        for (int r = 0; r < n; r++) {
+        for( int r = 0; r < n; r++ ){
 
-            while (!maxD.isEmpty() && nums[maxD.peekLast()] <= nums[r]) {
+            while( !maxD.isEmpty() && nums[ maxD.peekLast() ] <= nums[r] ){
                 maxD.pollLast();
             }
             maxD.offerLast(r);
 
-            while (!minD.isEmpty() && nums[minD.peekLast()] >= nums[r]) {
+            while( !minD.isEmpty() && nums[ minD.peekLast() ] >= nums[r] ){
                 minD.pollLast();
             }
             minD.offerLast(r);
 
-            while (nums[maxD.peekFirst()] - nums[minD.peekFirst()] > k) {
+            while( nums[ maxD.peekFirst() ] - nums[ minD.peekFirst() ] > k ){
 
-                if (maxD.peekFirst() == l) maxD.pollFirst();
-                if (minD.peekFirst() == l) minD.pollFirst();
+                if( maxD.peekFirst() == l ) maxD.pollFirst();
+                if( minD.peekFirst() == l ) minD.pollFirst();
 
-                trie.remove(pref[l]);
+                if( l == 0 )
+                    trie.remove( 0 );
+                else
+                    trie.remove( prefixXor[l-1] );
                 l++;
+
             }
 
-            ans = Math.max(ans, trie.search(pref[r + 1]));
+            int best = trie.search( prefixXor[r] );
+            ans = Math.max( best,ans );
+            trie.insert(prefixXor[r]);
 
-            trie.insert(pref[r + 1]);
         }
 
         return ans;
+
     }
 }
 
 class Trie{
-
     class TreeNode{
+        int val; 
         int count;
-        int val;
-        TreeNode left;
-        TreeNode right;
+        TreeNode left; // represents 0
+        TreeNode right;// represents 1
     }
 
     TreeNode root;
 
-    Trie(){
+    public Trie(){
         root = new TreeNode();
     }
-
 
     void insert( int num ){
         TreeNode crawl = root;
         for( int i = 31; i >= 0; i-- ){
-            int bit = ( (num>>i) & 1 );
+            int bit = ( num >> i ) & 1;
             if( bit == 0 ){
                 if( crawl.left == null ){
                     crawl.left = new TreeNode();
@@ -89,18 +91,12 @@ class Trie{
     void remove( int num ){
         TreeNode crawl = root;
         for( int i = 31; i >= 0; i-- ){
-            int bit = ( (num>>i) & 1 );
+            int bit = ( num >> i ) & 1;
             if( bit == 0 ){
-                if( crawl.left == null ){
-                    crawl.left = new TreeNode();
-                }
                 crawl = crawl.left;
                 crawl.count--;
             }
             else{
-                if( crawl.right == null ){
-                    crawl.right = new TreeNode();
-                }
                 crawl = crawl.right;
                 crawl.count--;
             }
@@ -110,23 +106,25 @@ class Trie{
     int search( int num ){
         TreeNode crawl = root;
         for( int i = 31; i >= 0; i-- ){
-            int bit = ( (num>>i) & 1 );
+            int bit = ( num >> i ) & 1;
             if( bit == 0 ){
                 if( crawl.right != null && crawl.right.count > 0 ){
                     crawl = crawl.right;
                 }
-                else
+                else{
                     crawl = crawl.left;
+                }
             }
             else{
                 if( crawl.left != null && crawl.left.count > 0 ){
                     crawl = crawl.left;
                 }
-                else
+                else{
                     crawl = crawl.right;
+                }
             }
         }
-
         return num ^ crawl.val;
     }
 }
+
